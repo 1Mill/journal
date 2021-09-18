61825b554833b9d6da21bb82d4434e1bedcb0597
1Mill <erik.ekberg@outlook.com>
2021-09-18 16:18:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 
-const SUPPORTED_TYPES = ['mongo']
+const SUPPORTED_TYPES = ['mongodb']
 
 const fetchNodeEnv = name => process && process.env && process.env[name]
 
@@ -25,7 +25,7 @@ class Journal {
 		this.client = undefined
 
 		// * Run immediately
-		if (!this.type === 'mongo') this._setupMongoIndexes()
+		if (!this.type === 'mongodb') this._setupMongoIndexes()
 	}
 
 	async _collection() {
